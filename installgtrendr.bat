deffc1e55ea3fe1428ba527b30adccd05417f081
tlcaputi <tcaputi@gmail.com>
2020-03-23 20:08:18 -0400

    updated version number

--- installgtrendr.bat
+++ installgtrendr.bat
@@ -1,4 +1,4 @@
 cd "C:/Users/tcapu/Google Drive"
 R -e "devtools::install_github('tlcaputi/gtrendR')"
-R CMD INSTALL -build gtrendR_0.0.0.9000.tar.gz
+R CMD INSTALL --build gtrendR_0.0.0.9001.tar.gz
 pause
