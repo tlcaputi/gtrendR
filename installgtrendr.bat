7f1978465af0164bc7bb371375edd6cf3d91a9c2
tlcaputi <tcaputi@gmail.com>
2020-03-24 00:39:05 -0400

    update readme

--- installgtrendr.bat
+++ installgtrendr.bat
@@ -1,4 +1,4 @@
 cd "C:/Users/tcapu/Google Drive"
 R -e "devtools::install_github('tlcaputi/gtrendR')"
-R CMD INSTALL --build gtrendR_0.0.0.9002.tar.gz
+R CMD INSTALL --build gtrendR_0.0.1.0000.tar.gz
 pause
--- readmetest.R
+++ readmetest.R
@@ -1,38 +1,24 @@
-# gtrendR: An R Complement to the gtrend package for Python
+pacman::p_load(
+  lubridate,
+  dplyr,
+  ggplot2,
+  forecast,
+  scales,
+  cowplot,
+  reshape2,
+  usmap
+)
 
-## Using the Package
-
-Before you begin using this package, pull the Google Trends data using the gtrends package for Python.
-
-### Installing the package
-
-```r
-
-##Only run this once
-library(devtools)
-devtools::install_github("tlcaputi/gtrendR", force = T)
-
-## Run this every time
 library("gtrendR")
 
-```
+setwd("C:/Users/tcapu/Google Drive/PublicHealthStudies/tob21searches")
 
-### ARIMA Spike with One Geography
-
-First, use `run_arima` to create a dataset in the correct format for other functions.
-
-```r
 US_df <- run_arima(
   df = read.csv("./output/data.csv", header = T, stringsAsFactor = F),
   interrupt = "2019-12-19",
   geo = "US"
 )
-```
 
-Now, you're ready to produce a few interesting figures. The first figure is a simple line plot.
-
-
-```r
 panA <- line_plot(
   US_df,
   beginplot = T,
@@ -49,11 +35,7 @@ panA <- line_plot(
   width = 6,
   outfn = './output/panA.png'
 )
-```
 
-You can also produce a plot that highlights the difference between the ARIMA-expected and actual search volumes.
-
-```r
 panB <- arima_plot(
   US_df,
   title = NULL,
@@ -70,11 +52,8 @@ panB <- arima_plot(
   height = 3,
   outfn = './output/panB.pdf'
 )
-```
 
-Finally, you can merge the plots together to create a single figure.
 
-```r
 title <- ggdraw() +
   draw_label(
     "Google Searches for Buying Tobacco Online",
@@ -88,17 +67,12 @@ title <- ggdraw() +
 fig <- plot_grid(panA, panB, labels=c(LETTERS[1:2]), ncol=1, nrow=2, rel_heights=c(1,1))
 fig <- plot_grid(title, fig, ncol = 1, rel_heights = c(0.1, 1))
 save_plot("./output/Fig1.png", fig, base_width=6, base_height=6)
-```
-
-![arima-spike-onegeo](images/Fig1.png)
 
 
-### ARIMA Spike with Multiple Geographies
-
-If you are interested in visualising changes by US state, you may want to create a figure showing the percentage change before versus after the interruption.
 
 
-```r
+
+setwd("C:/Users/tcapu/Google Drive/PublicHealthStudies/buyguns")
 
 panC <- pct_change_state(
   df = read.csv("./temp/data.csv", header = T, stringsAsFactor = F),
@@ -110,29 +84,21 @@ panC <- pct_change_state(
   linecol = "gray",
   lowcol = "red",
   midcol = "white",
-  highcol = "dodgerblue4"
+  highcol = "dodgerblue4",
+  save = T,
+  width = 6,
+  height = 3,
+  outfn = './output/panC.pdf'
 )
 
-```
-
-
-To show how states differ from their individual ARIMA estimates, start with `state_arima`.
-
-```r
 state_df <- state_arima(
   data = read.csv("./temp/data.csv", header = T, stringsAsFactor = F),
-  interrupt = "2019-03-01"
+  interrupt = "2020-03-01"
 )
-```
 
-
-Using the output from this, you can create a spaghetti plot showing the percent difference between the ARIMA-fitted values and the actual values.
-
-
-```r
 panD <- state_arima_spaghetti(
   state_df,
-  interrupt = "2019-03-01",
+  interrupt = "2020-03-01",
   title = NULL,
   xlab = "Date",
   ylab = "Actual Versus Model-Fitted\nSearch Queries (% Diff.)",
@@ -140,34 +106,37 @@ panD <- state_arima_spaghetti(
   linelabel = "COVID-19\nOutbreak",
   lbreak = "1 week",
   lwd = 0.4,
-  beginplot = ymd("2019-03-01")-(7*1),
-  endplot = ymd("2019-03-18"),
+  beginplot = ymd("2020-03-01")-(7*1),
+  endplot = ymd("2020-03-22"),
   xfmt = date_format("%d %b"),
   states_with_labels = c("CA", "NY", "US", "IA"),
   states_to_exclude = c("IA"),
   save = T,
   width = 6,
   height = 4,
-  outfn = "./output/panB.png"
+  outfn = "./output/panD.png"
 )
-```
 
 
-You can also visualize the state-specific differences between ARIMA-fitted values and actual values using `state_arima_pctdiff`.
-
-```r
 panE <- state_arima_pctdiff(
   state_df,
-  outfn = "./output/panC.png"
+  save = T
+  width = 6,
+  height = 3,
+  outfn = './output/panE.pdf',
 )
-```
-
-Finally, combine the plots.
 
 
-```r
+title <- ggdraw() +
+  draw_label(
+    "Google Searches for Purchasing Guns",
+    fontface = 'bold',
+    hjust = 0.5
+  ) +
+  theme(
+    plot.margin = margin(0, 0, 0, 7)
+  )
+
 fig <- plot_grid(panC, panD, panE, labels=c(LETTERS[3:5]), ncol=1, nrow=3, rel_heights=c(1.1, 1, 1.1))
+fig <- plot_grid(title, fig, ncol = 1, rel_heights = c(0.1, 1))
 save_plot("./output/Fig2.png", fig, base_width=6, base_height=9)
-```
-
-![arima-spike-multigeo](images/Fig2.png)
