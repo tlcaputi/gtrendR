7f76af9bbb8c9e702eff795a13133d84170d492f
tlcaputi <tcaputi@gmail.com>
2020-05-06 18:32:36 -0400

    added to and renamed utils

--- R/utils.R
+++ R/utils.R
@@ -167,3 +167,99 @@ minpos <- function(x) {
   # This makes it so that any numeric value is at least positive.
   return(ifelse(x <= 0, 0.00000001, x))
 }
+
+
+#' annotation_compass: taken from 
+#' https://stackoverflow.com/questions/47916307/ggplot2-specify-geom-text-position-by-conventional-top-bottom-left-rig
+#'
+#' @param label label
+#' @param position position
+#' @keywords
+#' @export
+#' @examples
+
+
+
+annotation_compass <- function(label,
+                               position = c('N','NE','E','SE','S','SW','W','NW'),
+                               padding = grid::unit(c(0.5,0.5),"line"), ...){
+  position <- match.arg(position)
+  x <- switch (position,
+    N = 0.5,
+    NE = 1,
+    E = 1,
+    SE = 1,
+    S = 0.5, 
+    SW = 0,
+    W = 0, 
+    NW = 0
+  )
+  y <- switch (position,
+               N = 1,
+               NE = 1,
+               E = 0.5,
+               SE = 0,
+               S = 0, 
+               SW = 0,
+               W = 0.5, 
+               NW = 1
+  )
+  hjust <- switch (position,
+               N = 0.5,
+               NE = 1,
+               E = 1,
+               SE = 1,
+               S = 0.5, 
+               SW = 0,
+               W = 0, 
+               NW = 0
+  )
+  vjust <- switch (position,
+               N = 1,
+               NE = 1,
+               E = 0.5,
+               SE = 0,
+               S = 0, 
+               SW = 0,
+               W = 0.5, 
+               NW = 1
+  )
+  f1 <- switch (position,
+                   N = 0,
+                   NE = -1,
+                   E = -1,
+                   SE = -1,
+                   S = 0, 
+                   SW = 1,
+                   W = 1, 
+                   NW = 1
+  )
+  f2 <- switch (position,
+                   N = -1,
+                   NE = -1,
+                   E = 0,
+                   SE = 1,
+                   S = 1, 
+                   SW = 1,
+                   W = 0, 
+                   NW = -1
+  )
+  annotation_custom(grid::textGrob(label, 
+                                   x=grid::unit(x,"npc") + f1*padding[1] , 
+                                   y=grid::unit(y,"npc") + f2*padding[2],
+                                   hjust=hjust,vjust=vjust, ...))
+}
+
+
+#' ceiling_dec: taken from 
+#' https://stackoverflow.com/questions/35807523/r-decimal-ceiling
+#'
+#' @param label label
+#' @param position position
+#' @keywords
+#' @export
+#' @examples
+
+
+ceiling_dec <- function(x, level=1) round(x + 5*10^(-level-1), level)
+
